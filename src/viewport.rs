40ab8e3cc9fd61c40c7d836f5655988f77b565ed
0x0nyx98 <nyxmailbox@proton.me>
2024-12-17 17:35:12 -0500

    ok . this needs some refactoring >w<

---
+++ src/viewport.rs
@@ -0,0 +1,63 @@
+use crate::*;
+
+impl HibachiApp {
+    pub fn paint_sprite(&self, pntr: &egui::Painter, o: Vec2, x: isize, y: isize, spr: MarioGraphics, pal: MarioColorPalette) {
+        let sprdata = &self.game_graphics[&spr];
+
+        pntr.image(egui::TextureId::from(&sprdata.slices[0]), egui::Rect::from_points(&[
+            egui::pos2((x + 2 * sprdata.offset[0]) as f32, (y + 2 * sprdata.offset[1]) as f32),
+            egui::pos2((x + 2 * sprdata.offset[0] + 2 * sprdata.size[0] as isize) as f32, (y + 2 * sprdata.offset[1] + 2 * sprdata.size[1] as isize) as f32)
+        ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&sprdata.palette][0]);
+
+        pntr.image(egui::TextureId::from(&sprdata.slices[1]), egui::Rect::from_points(&[
+            egui::pos2((x + 2 * sprdata.offset[0]) as f32, (y + 2 * sprdata.offset[1]) as f32),
+            egui::pos2((x + 2 * sprdata.offset[0] + 2 * sprdata.size[0] as isize) as f32, (y + 2 * sprdata.offset[1] + 2 * sprdata.size[1] as isize) as f32)
+        ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&sprdata.palette][1]);
+
+        pntr.image(egui::TextureId::from(&sprdata.slices[2]), egui::Rect::from_points(&[
+            egui::pos2((x + 2 * sprdata.offset[0]) as f32, (y + 2 * sprdata.offset[1]) as f32),
+            egui::pos2((x + 2 * sprdata.offset[0] + 2 * sprdata.size[0] as isize) as f32, (y + 2 * sprdata.offset[1] + 2 * sprdata.size[1] as isize) as f32)
+        ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&sprdata.palette][2]);
+    }
+
+    pub fn paint_silhouette(&self, pntr: &egui::Painter, o: Vec2, x: isize, y: isize, spr: MarioGraphics, pal: MarioColorPalette) {
+        let sprdata = &self.game_graphics[&spr];
+
+        pntr.image(egui::TextureId::from(&sprdata.slices[0]), egui::Rect::from_points(&[
+            egui::pos2((x + 2 * sprdata.offset[0]) as f32, (y + 2 * sprdata.offset[1]) as f32),
+            egui::pos2((x + 2 * sprdata.offset[0] + 2 * sprdata.size[0] as isize) as f32, (y + 2 * sprdata.offset[1] + 2 * sprdata.size[1] as isize) as f32)
+        ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&sprdata.palette][0]);
+
+        pntr.image(egui::TextureId::from(&sprdata.slices[1]), egui::Rect::from_points(&[
+            egui::pos2((x + 2 * sprdata.offset[0]) as f32, (y + 2 * sprdata.offset[1]) as f32),
+            egui::pos2((x + 2 * sprdata.offset[0] + 2 * sprdata.size[0] as isize) as f32, (y + 2 * sprdata.offset[1] + 2 * sprdata.size[1] as isize) as f32)
+        ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&sprdata.palette][0]);
+
+        pntr.image(egui::TextureId::from(&sprdata.slices[2]), egui::Rect::from_points(&[
+            egui::pos2((x + 2 * sprdata.offset[0]) as f32, (y + 2 * sprdata.offset[1]) as f32),
+            egui::pos2((x + 2 * sprdata.offset[0] + 2 * sprdata.size[0] as isize) as f32, (y + 2 * sprdata.offset[1] + 2 * sprdata.size[1] as isize) as f32)
+        ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&sprdata.palette][0]);
+    }
+
+    pub fn paint_tag(&self, pntr: &egui::Painter, o: Vec2, x: isize, y: isize, txt: &str, pal: MarioColorPalette) {
+        let mut i = 0;
+
+        pntr.rect_filled(
+            egui::Rect::from_points(&[
+                egui::pos2((x) as f32, y as f32),
+                egui::pos2((x + 16 * (txt.len() + 1) as isize) as f32, (y + 16 as isize) as f32)
+            ]).translate(o),
+            egui::Rounding::ZERO,
+            self.game_palette[&pal][&MarioColor::BlackEnemy][2],
+        );
+        
+        for c in txt.to_uppercase().chars() {
+            pntr.image(egui::TextureId::from(&(self.game_font)[&c].slices[0]), egui::Rect::from_points(&[
+                egui::pos2((x + 16 * i) as f32, y as f32),
+                egui::pos2((x + 16 * (i + 1)) as f32, (y + 16 as isize) as f32)
+            ]).translate(o), egui::Rect{min: egui::pos2(0.0, 0.0), max: egui::pos2(1.0, 1.0)}, self.game_palette[&pal][&MarioColor::BlackEnemy][1]);
+    
+            i += 1;
+        }
+    }
+}
